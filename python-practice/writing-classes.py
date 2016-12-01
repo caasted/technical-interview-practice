3125c650f18ef7dce20e8e3dda5d14c9772ccbe4
Chris Aasted <caasted@gmail.com>
2016-12-01 12:16:42 -0800

    feat: Add making-classes.py

---
+++ python-practice/writing-classes.py
@@ -0,0 +1,57 @@
+# From Udacity Full Stack Web Developer Nanodegree
+
+"""You can use this class to represent how classy someone
+or something is.
+"Classy" is interchangable with "fancy".
+If you add fancy-looking items, you will increase
+your "classiness".
+Create a function in "Classy" that takes a string as
+input and adds it to the "items" list.
+Another method should calculate the "classiness"
+value based on the items.
+The following items have classiness points associated
+with them:
+"tophat" = 2
+"bowtie" = 4
+"monocle" = 5
+Everything else has 0 points.
+Use the test cases below to guide you!"""
+
+class Classy(object):
+    def __init__(self):
+        self.items = []
+        
+    def getClassiness(self):
+        classiness = 0
+        for word in self.items:
+            if word is "tophat":
+                classiness += 2
+            if word is "bowtie":
+                classiness += 4
+            if word is "monocle":
+                classiness += 5
+        return classiness
+    
+    def addItem(self, item):
+        if item:
+            self.items.append(item)
+
+# Test cases
+me = Classy()
+
+# Should be 0
+print me.getClassiness()
+
+me.addItem("tophat")
+# Should be 2
+print me.getClassiness()
+
+me.addItem("bowtie")
+me.addItem("jacket")
+me.addItem("monocle")
+# Should be 11
+print me.getClassiness()
+
+me.addItem("bowtie")
+# Should be 15
+print me.getClassiness()

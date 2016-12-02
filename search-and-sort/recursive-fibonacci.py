0941ce8fa7136904e3a574a5ba3508a247612510
Chris Aasted <caasted@gmail.com>
2016-12-02 11:47:09 -0800

    feat: Simplify recursive-fibonacci slightly

--- search-and-sort/recursive-fibonacci.py
+++ search-and-sort/recursive-fibonacci.py
@@ -13,8 +13,7 @@ def get_fib(position):
     if position == 1:
         return 1
     else:
-        output = get_fib(position - 1) + get_fib(position - 2)
-        return output
+        return get_fib(position - 1) + get_fib(position - 2)
 
 # Test cases
 print get_fib(9)

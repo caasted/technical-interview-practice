2bc8e7d705f824418ef35840885946832c5f0a97
Chris Aasted <caasted@gmail.com>
2017-01-10 17:58:09 -0800

    feat: Add softmax.py to deep-learning

---
+++ deep-learning/softmax.py
@@ -0,0 +1,50 @@
+# softmax(yi) = exp(yi) / sum_j(exp(yj))
+
+import numpy
+import math
+
+def softmax(x):
+	array = numpy.transpose(x)
+	shape = array.shape
+	if len(shape) == 1:
+		print "1D Array"
+		temp = []
+		for value in x:
+			temp.append(math.exp(value))
+		
+		sum_of_x = sum(temp)
+		
+		result = []
+		for value in temp:
+			result.append(value / sum_of_x)
+		
+		return result
+	elif len(shape) == 2:
+		print "2D Array"
+		rows = shape[0]
+		cols = shape[1]
+		result = []
+		for row in range(rows):
+			temp1 = []
+			for col in range(cols):
+				temp1.append(math.exp(x[col][row]))
+
+			sum_of_x = sum(temp1)
+			
+			temp2 = []
+			for value in temp1:
+				temp2.append(value / sum_of_x)
+
+			result.append(temp2)
+		return numpy.transpose(result)
+	else:
+		return None
+
+
+scores = [1.0, 2.0, 3.0]
+
+print softmax(scores), "\n"
+
+scores = [[1, 2, 3, 6], [2, 4, 5, 6], [3, 8, 7, 6]]
+
+print softmax(scores)
